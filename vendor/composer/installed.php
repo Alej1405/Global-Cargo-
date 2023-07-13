32d8bfebdddb709a935bbcd3195e9aab3bf7019a
Alej1405 <103963976+Alej1405@users.noreply.github.com>
2023-07-13 13:32:53 -0500

    agregando phpmailer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alejandro/assets',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0ee16c9f4ac2e67d6d57fa835b781c540b0fef7b',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'alejandro/assets' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0ee16c9f4ac2e67d6d57fa835b781c540b0fef7b',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.8.0',
+            'version' => '6.8.0.0',
+            'reference' => 'df16b615e371d81fb79e506277faea67a1be18f1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
